a5fef03cefc598998a8a45fcf48408610dcde6b9
Ivy-Walobwa <ivywalobwa@gmail.com>
2020-08-24 17:15:34 +0300

    add fonts

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -10,7 +10,7 @@ class ProductsOverviewScreen extends StatelessWidget {
       description: 'A red shirt - it is pretty red!',
       price: 29.99,
       imageUrl:
-      'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
     ),
     Product(
       id: 'p2',
@@ -18,7 +18,7 @@ class ProductsOverviewScreen extends StatelessWidget {
       description: 'A nice pair of trousers.',
       price: 59.99,
       imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
     ),
     Product(
       id: 'p3',
@@ -26,7 +26,7 @@ class ProductsOverviewScreen extends StatelessWidget {
       description: 'Warm and cozy - exactly what you need for the winter.',
       price: 19.99,
       imageUrl:
-      'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
     ),
     Product(
       id: 'p4',
@@ -34,7 +34,7 @@ class ProductsOverviewScreen extends StatelessWidget {
       description: 'Prepare any meal you want.',
       price: 49.99,
       imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
     ),
   ];
 
@@ -45,17 +45,21 @@ class ProductsOverviewScreen extends StatelessWidget {
         title: Text('MyShop'),
       ),
       body: GridView.builder(
-        padding: EdgeInsets.all(10.0),
+        padding: EdgeInsets.all(20.0),
         itemCount: loadedProducts.length,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20,
-            childAspectRatio: 3 / 2),
-        itemBuilder: (ctx, index) =>
-            ProductItem(title: loadedProducts[index].title,
-              id: loadedProducts[index].id,
-              imageUrl: loadedProducts[index].imageUrl,),),
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          childAspectRatio: 2/2.5
+            ),
+        itemBuilder: (ctx, index) => ProductItem(
+          title: loadedProducts[index].title,
+          id: loadedProducts[index].id,
+          imageUrl: loadedProducts[index].imageUrl,
+          price: loadedProducts[index].price,
+        ),
+      ),
     );
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -5,37 +5,42 @@ class ProductItem extends StatelessWidget {
   final String id;
   final String imageUrl;
   final String title;
+  final double price;
 
   ProductItem({
     this.imageUrl,
     this.title,
     this.id,
+    this.price,
   });
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
-        repeat: ImageRepeat.noRepeat,
-      ),
-      header: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          SizedBox(),
-          IconButton(
-            icon: Icon(Icons.favorite),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
+        header: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            SizedBox(),
+            IconButton(
+              icon: Icon(Icons.favorite_border),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          title: Text(title),
+          subtitle: Text('\$$price'),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
             onPressed: () {},
           ),
-        ],
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        title: Text(title),
-        trailing: IconButton(
-          icon: Icon(Icons.shopping_cart),
-          onPressed: () {},
         ),
       ),
     );
