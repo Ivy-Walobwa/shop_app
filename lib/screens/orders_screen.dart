b1aaa32fadf94053e2de76f4020f269826fb1ce3
Ivy-Walobwa <ivywalobwa@gmail.com>
2020-09-01 11:42:42 +0300

    using future builder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -3,47 +3,36 @@ import 'package:provider/provider.dart';
 import 'package:shop_app/providers/orders.dart';
 import 'package:shop_app/widgets/order_item_widget.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
 
   @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((value) async{
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchOrdersFromDB();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Your Orders',
         ),
       ),
-      body: _isLoading ? Center(child: CircularProgressIndicator()): ListView.builder(
-        itemBuilder: (ctx, i) =>
-            OrderItemWidget(
-              order: orderData.orders[i],
-            ),
-        itemCount: orderData.orders.length,
-      ),
+      body: FutureBuilder(future: Provider.of<Orders>(context, listen: false).fetchOrdersFromDB(), builder: (ctx, snapshot){
+        if(snapshot.connectionState == ConnectionState.waiting){
+         return Center(child: CircularProgressIndicator());
+        }else{
+          if(snapshot.error != null){
+            return Center(child: Text('An error occurred'),);
+          }else{
+            return Consumer<Orders>(builder: (ctx, orderData, child)=>
+              ListView.builder(
+                itemBuilder: (ctx, i) =>
+                    OrderItemWidget(
+                      order: orderData.orders[i],
+                    ),
+                itemCount: orderData.orders.length,
+              )
+            ,);
+          }
+        }
+      },)
     );
   }
 
