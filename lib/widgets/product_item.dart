ed609f19a8685905b95c8c0efd0d58d4b444cb00
Ivy-Walobwa <ivywalobwa@gmail.com>
2020-10-09 21:58:58 +0300

    hero transition

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -21,10 +21,13 @@ class ProductItem extends StatelessWidget {
             Navigator.pushNamed(context, ProductDetailScreen.routeName,
                 arguments: product.id);
           },
-          child: FadeInImage(
-            placeholder: AssetImage('assets/images/product-placeholder.png'),
-            image: NetworkImage(product.imageUrl),
-            fit: BoxFit.cover,
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/product-placeholder.png'),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
         header: Row(
