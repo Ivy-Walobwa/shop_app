427a26aceb3ccefcca7679a1509fb7ba1507453b
Ivy-Walobwa <ivywalobwa@gmail.com>
2020-10-09 21:42:32 +0300

    transition

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -104,36 +104,30 @@ class _AuthCardState extends State<AuthCard>
   var _isLoading = false;
   final _passwordController = TextEditingController();
 
-//  AnimationController _controller;
-//  Animation<Size> _heightAnimation;
-//
-//  @override
-//  void initState() {
-//    _controller =
-//        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
-//    _heightAnimation = Tween<Size>(
-//      begin: Size(
-//        double.infinity,
-//        260,
-//      ),
-//      end: Size(
-//        double.infinity,
-//        320,
-//      ),
-//    ).animate(
-//      CurvedAnimation(
-//        parent: _controller,
-//        curve: Curves.fastOutSlowIn,
-//      ),
-//    );
-//    super.initState();
-//  }
-//
-//  @override
-//  void dispose() {
-//    _controller.dispose();
-//    super.dispose();
-//  }
+  AnimationController _controller;
+  Animation<double> _opacityAnimation;
+
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    _opacityAnimation = Tween<double>(
+      begin:0.0,
+      end:1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeIn,
+      ),
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   void showErrorDialog(String message) {
     showDialog(
@@ -199,12 +193,12 @@ class _AuthCardState extends State<AuthCard>
     if (_authMode == AuthMode.Login) {
       setState(() {
         _authMode = AuthMode.Signup;
-//        _controller.forward();
+        _controller.forward();
       });
     } else {
       setState(() {
         _authMode = AuthMode.Login;
-//        _controller.reverse();
+        _controller.reverse();
       });
     }
   }
@@ -255,18 +249,25 @@ class _AuthCardState extends State<AuthCard>
                     _authData['password'] = value;
                   },
                 ),
-                if (_authMode == AuthMode.Signup)
-                  TextFormField(
-                    enabled: _authMode == AuthMode.Signup,
-                    decoration: InputDecoration(labelText: 'Confirm Password'),
-                    obscureText: true,
-                    validator: _authMode == AuthMode.Signup
-                        ? (value) {
-                            if (value != _passwordController.text) {
-                              return 'Passwords do not match!';
-                            }
-                          }
-                        : null,
+                  AnimatedContainer(
+                    constraints: BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 60 : 0,maxHeight: _authMode == AuthMode.Signup ? 120 : 0),
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.easeIn,
+                    child: FadeTransition(
+                      opacity: _opacityAnimation,
+                      child: TextFormField(
+                        enabled: _authMode == AuthMode.Signup,
+                        decoration: InputDecoration(labelText: 'Confirm Password'),
+                        obscureText: true,
+                        validator: _authMode == AuthMode.Signup
+                            ? (value) {
+                                if (value != _passwordController.text) {
+                                  return 'Passwords do not match!';
+                                }
+                              }
+                            : null,
+                      ),
+                    ),
                   ),
                 SizedBox(
                   height: 20,
